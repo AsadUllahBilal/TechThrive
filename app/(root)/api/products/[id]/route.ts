2e10fa3a2e3d4c09424b0acdfa9e8718133e0293
Asad Ullah <bilalasaf998@gmail.com>
2025-08-16 03:01:02 +0500

    update api

--- app/(root)/api/products/[id]/route.ts
+++ app/(root)/api/products/[id]/route.ts
@@ -2,19 +2,6 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongo";
 import Product from "@/models/product.model";
 
-// GET single product
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  await connectDB();
-  const product = await Product.findById(params.id);
-  if (!product) {
-    return NextResponse.json({ error: "Product not found" }, { status: 404 });
-  }
-  return NextResponse.json(product);
-}
-
 // UPDATE product
 export async function PUT(
   req: Request,
